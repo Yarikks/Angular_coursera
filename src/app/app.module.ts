43d847370dd4ddda560f697ce17fc2e2598811aa
Yarikks <jasper18poil@gmail.com>
2020-04-17 10:44:40 +0300

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './service/dish.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/service/dish.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
+
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DishService {
+
+  constructor() { }
+
+  getDishes(): Dish[] {
+    return DISHES;
+  }
+}
